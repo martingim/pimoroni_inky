8a1b210b690539cce27d2d92935ff44a5903cb74
martingim <martingimse@gmail.com>
2025-01-20 14:05:47 +0100

    clock and weather icon only

---
+++ clock_weather_timer.py
@@ -0,0 +1,68 @@
+from time import sleep, localtime, strftime
+from inky.auto import auto, InkyWHAT
+from PIL import Image, ImageFont, ImageDraw
+from matplotlib import font_manager
+what_font_size = 110
+phat_font_size = 40
+
+## Inky display setup
+try:
+    inky_display = auto(ask_user=True, verbose=True)
+except TypeError:
+    raise TypeError("You need to update the Inky library to >= v1.1.0")
+
+#check for what and set to black mode and larger fontsize
+if inky_display.height == 300:
+    inky_display = InkyWHAT("black")
+    fontsize = what_font_size
+else:
+     fontsize = phat_font_size
+
+try:
+    inky_display.set_border(inky_display.BLACK)
+except NotImplementedError:
+    pass
+
+
+## FONT SETUP
+font = font_manager.FontProperties(fname="~/usr/share/fonts/truetype/dseg/DSEG7Classic-Bold.ttf")
+SEG_file = font_manager.findfont(font)
+
+weather_icons = font_manager.FontProperties(fname="~/usr/share/fonts/truetype/dseg/DSEGWeather.ttf")
+icons_file = font_manager.findfont(weather_icons)
+
+ICONS_font = ImageFont.truetype(icons_file, fontsize)
+SEG_font = ImageFont.truetype(SEG_file, fontsize)
+
+
+def inky_txt(currtime, temperature, icon, display, text_font, icon_font):
+        img = Image.new("P", (display.WIDTH, display.HEIGHT),100)
+        img.paste(display.BLACK, (0,0,img.size[0],img.size[1]))
+        draw = ImageDraw.Draw(img)
+
+        #set the time
+        tmp, tmp, w, h = text_font.getbbox(currtime)
+        x = (display.WIDTH / 2) - (w/2)
+        y = (display.HEIGHT/2)  - (h/2)
+        draw.text((x,y), currtime, display.WHITE, text_font)
+        
+        #set the weather icon
+        tmp, tmp, w, h = icon_font.getbbox(icon)
+        x = display.WIDTH - w
+        y = display.HEIGHT -h
+        draw.text((x,y), icon, display.WHITE, icon_font)
+
+        display.set_image(img)
+        display.show()
+        print(currtime)
+
+icon = 0
+old_time = ''
+temperature = '-10.0'
+while True:
+    currtime = strftime("%H:%M", localtime())
+    if currtime!=old_time:
+        inky_txt(currtime,temperature, str(icon), inky_display, SEG_font, ICONS_font)
+    sleep(10)
+    old_time=currtime
+    icon += 1

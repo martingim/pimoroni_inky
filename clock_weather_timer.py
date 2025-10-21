111b73cc64c14747ad8249fcda6c59d225db5f29
Martin Gimse <42689774+martingim@users.noreply.github.com>
2025-10-21 18:19:58 +0200

    added work timer

--- clock_weather_timer.py
+++ clock_weather_timer.py
@@ -1,6 +1,10 @@
 from metno_locationforecast import Forecast, Place
+from gpiozero import Button
+from RPi import GPIO    
+from signal import pause
+from threading import Thread
 from datetime import datetime, timezone, timedelta
-from time import sleep, localtime, strftime
+from time import sleep, localtime, strftime, gmtime, time
 from inky.auto import auto, InkyWHAT
 from PIL import Image, ImageFont, ImageDraw
 from matplotlib import font_manager
@@ -48,6 +52,7 @@ temp_font = ImageFont.truetype(SEG_file, int(fontsize*0.5))
 p_font = ImageFont.truetype(SEG_file, int(fontsize*0.25))
 uv_font = ImageFont.truetype(icons_file, int(fontsize*0.5))
 
+#weather text
 def inky_txt(currtime, temperature, precipitation, icon, uv_symbol, display):
         img = Image.new("P", (display.WIDTH, display.HEIGHT),100)
         img.paste(display.BLACK, (0,0,img.size[0],img.size[1]))
@@ -85,6 +90,37 @@ def inky_txt(currtime, temperature, precipitation, icon, uv_symbol, display):
         display.show()
         print(currtime)
 
+#timer time
+def txtwhat(msg, display, font):
+        img = Image.new("P", (display.WIDTH, display.HEIGHT),100)
+        img.paste(display.BLACK, (0,0,img.size[0],img.size[1]))
+        draw = ImageDraw.Draw(img)
+
+        w, h = font.getsize(msg)
+        x = (display.WIDTH / 2) - (w/2)
+        y = (display.HEIGHT/2)  - (h/2)
+        draw.text((x,y), msg, display.WHITE, font)
+        display.set_image(img)
+        display.show()
+        print(msg)
+
+def ylwtxtwhat(msg, font):
+        display = InkyWHAT("yellow")
+        display.set_border(display.BLACK)
+        img = Image.new("P", (display.WIDTH, display.HEIGHT),100)
+        img.paste(display.BLACK, (0,0,img.size[0],img.size[1]))
+
+        draw = ImageDraw.Draw(img)
+
+
+        w, h = font.getsize(msg)
+        x = (display.WIDTH / 2) - (w/2)
+        y = (display.HEIGHT/2)  - (h/2)
+        draw.text((x,y), msg, display.YELLOW, font)
+        display.set_image(img)
+        display.show()
+        print(msg, " Paused")
+
 ## WEATHER ##
 def utc_to_local(utc_dt):
     return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
@@ -99,47 +135,179 @@ except:
     home_forecast.update()
 
 
-icon = 0
-old_time = ''
-temperature = ''
-while True:
-    if utc.localize(home_forecast.data.expires)<datetime.now(timezone.utc):
-        print("expired, updating forecast")
-        home_forecast.update()
-    
-    interval = home_forecast.data.intervals[0]
-    variables = interval.variables
-    t = variables["air_temperature"].value
-    temperature = f"{t}\u00b0"
-    data = json.loads(home_forecast.json_string)
-    symbol_code = data['data']['properties']['timeseries'][0]['data']['next_6_hours']['summary']['symbol_code']
+def weather_report():
+    icon = 0
+    old_time = ''
+    temperature = ''
 
-    #precipitation and uv next 6 hours
-    now = datetime.today()
-    now_plus_6_hours = now + timedelta(hours=6)
-    precipitation_min = 0
-    precipitation_max = 0
-    uv_max = 0
-    for interval in home_forecast.data.intervals_between(now, now_plus_6_hours):
-        precipitation_min += interval.variables["precipitation_amount_min"].value
-        precipitation_max += interval.variables["precipitation_amount_max"].value
-        uv_max = max(uv_max, interval.variables["ultraviolet_index_clear_sky"].value)    
-    if precipitation_max > 0:
-        precipitation = f"{precipitation_min:.1f}-{precipitation_max:.1f}"
-    else:
-        precipitation = ""
+    while True:
+        if utc.localize(home_forecast.data.expires)<datetime.now(timezone.utc):
+            print("expired, updating forecast")
+            home_forecast.update()
         
-    if uv_max > 2.9:
-        uv_symbol = '1'
-    else:
-        uv_symbol = ':'
-        
-    try:
-        icon = icon_dict[symbol_code]
-    except:
-        icon = ':'        
-    currtime = strftime("%H:%M", localtime())
-    if currtime!=old_time:
-        inky_txt(currtime,temperature, precipitation, icon, uv_symbol, inky_display)
-    sleep(10)
-    old_time=currtime
+        interval = home_forecast.data.intervals[0]
+        variables = interval.variables
+        t = variables["air_temperature"].value
+        temperature = f"{t}\u00b0"
+        data = json.loads(home_forecast.json_string)
+        symbol_code = data['data']['properties']['timeseries'][0]['data']['next_6_hours']['summary']['symbol_code']
+
+        #precipitation and uv next 6 hours
+        now = datetime.today()
+        now_plus_6_hours = now + timedelta(hours=6)
+        precipitation_min = 0
+        precipitation_max = 0
+        uv_max = 0
+        for interval in home_forecast.data.intervals_between(now, now_plus_6_hours):
+            precipitation_min += interval.variables["precipitation_amount_min"].value
+            precipitation_max += interval.variables["precipitation_amount_max"].value
+            uv_max = max(uv_max, interval.variables["ultraviolet_index_clear_sky"].value)    
+        if precipitation_max > 0:
+            precipitation = f"{precipitation_min:.1f}-{precipitation_max:.1f}"
+        else:
+            precipitation = ""
+            
+        if uv_max > 2.9:
+            uv_symbol = '1'
+        else:
+            uv_symbol = ':'
+            
+        try:
+            icon = icon_dict[symbol_code]
+        except:
+            icon = ':'        
+        currtime = strftime("%H:%M", localtime())
+        if currtime!=old_time:
+            inky_txt(currtime,temperature, precipitation, icon, uv_symbol, inky_display)
+        old_time=currtime
+
+
+################# work timer ###########################
+def run_timer():
+        global work_time
+        global remaining_time
+        global endtime
+        global Running
+        global Paused
+        while True:
+                message = strftime("%H:%M", gmtime(work_time))
+                display = InkyWHAT("black")
+                display.set_border(display.BLACK)
+
+                font = font_manager.FontProperties(fname="~/usr/share/fonts/truetype/dseg/DSEG7Classic-Bold.ttf")
+                file = font_manager.findfont(font)
+                font = ImageFont.truetype(file, 110)
+
+                endtime = time() + work_time
+                remaining_time = work_time
+                while remaining_time>0:
+                        txtwhat(message, display, font)
+                        #Check if it is time to update the display
+                        remaining_time = endtime - time()
+                        newtime = strftime("%H:%M", gmtime(remaining_time))
+                        tmp_r_t =  remaining_time
+                        while newtime == message:
+                                tmp_r_t = endtime - time()
+                                sleep(1)
+                                newtime = strftime("%H:%M", gmtime(tmp_r_t))
+                                #check if remaining time has changed while running
+                                if message != strftime("%H:%M", gmtime(remaining_time)):
+                                        newtime = strftime("%H:%M", gmtime(remaining_time))
+                                        break
+                                if Paused:
+                                    while Paused:
+                                        weather_report()
+                                        sleep(1)
+                                        #if time is added while paused
+                                        if message != strftime("%H:%M", gmtime(remaining_time)):
+                                            newtime = strftime("%H:%M", gmtime(remaining_time))
+                                            break
+                                    endtime = remaining_time + time()
+                        #update the message
+                        message = newtime
+                        remaining_time = endtime - time()
+                message = strftime("%H:%M", gmtime(0))
+                ylwtxtwhat(message, font)
+                Running = False
+                with open('times_worked.txt', 'a') as f:
+                        f.write(strftime("%d.%m.%y: ", localtime()) + str(work_time) + "\n")
+                work_time = 0
+                while work_time == 0:
+                        sleep(10)
+
+
+def Pause():
+        #Pause the timer
+        global Paused
+        Paused = True
+        print("paused\n")
+
+def unPause():
+        #unpause the timer
+        global Paused
+        Paused = False
+        print("unpaused\n")
+
+def add_10_min():
+        global work_time
+        global remaining_time
+        global endtime
+        work_time += 10*60
+        remaining_time += 10*60
+        endtime += 10*60
+        print("added 10 min")
+
+def sub_10_min():
+        global work_time
+        global remaining_time
+        global endtime
+        work_time -= 10*60
+        remaining_time -= 10*60
+        endtime -= 10*60
+        print("sub 10 min")
+
+def wait_for_input():
+        global Paused
+        global Running
+        while Running:
+                user_input = input("type p for pause u for unpause:\n")
+                if user_input == "p":
+                        print("PAUSED")
+                        Paused = True
+                elif user_input == "u":
+                        print("UNPAUSED")
+                        Paused = False
+                else:
+                        pass
+
+if __name__ == "__main__":
+
+        #Setup buttons
+        buttonfront = Button(12)
+        buttonrear = Button(16)
+        switch = Button(26)
+        #Get work time
+        try:
+                work_time_H = float(sys.argv[1])
+        except:
+                work_time_H = 0.5 #work time in hours
+
+        work_time = work_time_H*3600 + 59
+        remaining_time = work_time
+        #Start Running and check if the switch is on or off
+        Paused = not GPIO.input(26)
+        Running = True
+
+
+        t1 = Thread(target=run_timer)
+        t2 = Thread(target=wait_for_input)
+        t2.daemon = True
+        t1.start()
+        t2.start()
+
+        switch.when_pressed = Pause
+        switch.when_released = unPause
+        buttonrear.when_pressed = add_10_min
+        buttonfront.when_pressed = sub_10_min
+        pause()
+
